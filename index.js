4e235589afa74a00574f96addfd522d3bd251e29
Namisan <mongtamquoc2015@gmail.com>
2020-08-15 17:57:02 +0700

    Search the users

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ const users = require('./users.js');
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.get('/users', (req,res) => {
-	res.render('user/index', {users})
+app.get('/users', (req, res) => {
+	res.render('users/index', { users });
 });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/users/search', (req, res) => {
+	const q = req.query.q;
+	const matchedUsers = users.filter(user => {
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	})
+	res.render('users/index', { users: matchedUsers });
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Server started on port ${port}`));
