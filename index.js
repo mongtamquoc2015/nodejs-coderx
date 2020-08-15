c9a554776c49542177dbd381e1d08d884166ffe5
Namisan <mongtamquoc2015@gmail.com>
2020-08-15 17:38:27 +0700

    Render view with pug template

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const users = require('./users.js');
+
+// Template engine
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.get('/users', (req,res) => {
+	res.render('user/index', {users})
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port port!`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
